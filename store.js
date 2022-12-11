929acd9cfbf40304c8db23cbd44ef0c6d0ed18e1
Gabriel Marley <cardoso_eng@outlook.com>
2022-12-11 19:22:46 -0300

    botao remover js

--- store.js
+++ store.js
@@ -1,2 +1,9 @@
-var removeCartitemButtons = document.getElementbyClassname('remove-button')
-console.log(removeCartitemButtons)
+var cartRemoveButton = document.getElementsByClassName('item-remove-button')
+console.log(cartRemoveButton)
+for(var i = 0; i < cartRemoveButton.length; i++) {
+    var button = cartRemoveButton[i]
+    button.addEventListener('click', function(event){
+        var buttonClicked = event.target
+        buttonClicked.parentElement.parentElement.remove()
+    })
+}

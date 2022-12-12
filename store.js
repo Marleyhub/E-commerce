e2923a8a08e117845b23cdbedd847a4367eac770
Gabriel Marley <cardoso_eng@outlook.com>
2022-12-11 21:10:49 -0300

    addtopcart js button config

--- store.js
+++ store.js
@@ -1,9 +1,23 @@
+
+// Botão remover
+
 var cartRemoveButton = document.getElementsByClassName('item-remove-button')
 console.log(cartRemoveButton)
 for(var i = 0; i < cartRemoveButton.length; i++) {
     var button = cartRemoveButton[i]
     button.addEventListener('click', function(event){
         var buttonClicked = event.target
+        console.log(buttonClicked)
         buttonClicked.parentElement.parentElement.remove()
     })
+}
+
+
+// Botão commprar 
+
+var shopButton = document.getElementsByClassName('item-shop-button')
+console.log(shopButton)
+for(i = 0; i < shopButton.length; i++){
+    var button = shopButton[i]
+    button.addEventListener('click', shopbuttonclicked)
 }

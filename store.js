7e71c5cc955d56c9fce752cb6c26abfc9b10f32b
Gabriel Marley <cardoso_eng@outlook.com>
2022-12-16 17:20:11 -0300

    updating the total cart price

--- store.js
+++ store.js
@@ -6,7 +6,7 @@ for(var i = 0; i < cartRemoveButton.length; i++) {
     var button = cartRemoveButton[i]
     button.addEventListener('click', function(event){
         var buttonClicked = event.target
-        console.log(buttonClicked)
+        console.log('clicked')
         buttonClicked.parentElement.parentElement.remove()
     })
 }
@@ -27,4 +27,13 @@ for(i = 0; i < shopButton.length; i++){
         console.log(itemPrice, itemImg)
 
     }
-
+     
+ // Update do valor total 
+  var cartConteiner = document.getElementsbyClassName('shopping-cart-content')[0]
+  var cartRows = document.getElementsByClassName('cart-row')
+  for (i = 0; i < cartRow.length; i++){
+      var cartRow = cartRows.getElementsByClassName('cart-row')[0]
+      var itemPrice = cartRow.getElementsByClassName('item-price')
+      console.log(cartRow, itemPrice)
+  }
+  

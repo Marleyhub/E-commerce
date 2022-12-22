37f1d36628378390350be7f22b8f6f640b0e2a3f
Gabriel Marley <cardoso_eng@outlook.com>
2022-12-22 20:11:28 -0300

    ..

--- store.js
+++ store.js
@@ -75,7 +75,16 @@ function addToCart(event){
 
 function addRowToCart(itemPrice, imgSrc){
     var cartRow = document.createElement('div')
+    cartRow.classList.add('card-cart-row')
+    cartRow.classList.add('cart-row')
     var shoppingCart = document.getElementsByClassName('shopping-cart')[0]
     shoppingCart.append(cartRow)
-    cartRow.innerText = itemPrice
+    cartRowContent =
+        `
+        <img src="${imgSrc}" class="card-cart-img" alt="item.jpg">
+        <input type="number" class="cart-item-quantity" value="1">
+        <p class="cart-item-price">${itemPrice}</p>
+        <button type="submit" class="cart-remove-button">Remove</button>
+        `
+    cartRow.innerHTML = cartRowContent
 }
